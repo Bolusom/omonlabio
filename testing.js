b14b5af6526337b4cafb1f8301a3dd2b5777e9d3
Bolusom <155393114+Bolusom@users.noreply.github.com>
2024-04-28 10:14:44 +0100

    my bio

---
+++ testing.js
@@ -0,0 +1,51 @@
+const step1 = "collect pants";
+const step2 = "?";
+const step3 = "Profit";
+const total = step1 + step2 + step3;
+console.log("total steps:" + total);
+const correct = "is code correct?";
+console.log(correct + true)
+let condition = false;
+if (condition){
+    console.log("1");
+}
+else{
+    console.log("2");
+}
+// another example
+let isDay = true;
+if (isDay===true){
+    console.log("Lights off!");
+}
+// or ;et say
+let isDay = false;
+if(isDay){
+    console.log("Lights off!");
+}
+else {
+    console.log("lights on!");
+}
+// another example
+let number=99;
+if (number===1){
+    console.log("it is 1");
+}
+else{
+    console.log("it is not 1!");
+}
+// another 
+let choosenNumber = 4;
+if (choosenNumber===9){
+    console.log("you have gussed right");
+}
+else{
+    console.log("you have another chance to guess");
+}
+// another one
+let hour = 9;
+if (hour<12){
+    console.log("good morning");
+}
+else {
+    console.log("good afternoon");
+}
